c43ec699c3629dfe705d6d1fdeed355689136465
Kyle Aleshire <kjaleshire@gmail.com>
2014-12-18 13:35:36 -0600

    syntax fixes

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 
 func main() {
     port := os.Getenv("PORT")
-    if port = "" {
+    if port == "" {
         port = "8080"
     }
 
-    http.HandleFunc("/markdown" GenerateMarkdown)
+    http.HandleFunc("/markdown", GenerateMarkdown)
     http.Handle("/", http.FileServer(http.Dir("public")))
     http.ListenAndServe(":" + port, nil)
 }
