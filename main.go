21391738eb3ed90893c684c826fc5208d41bddde
Kyle Aleshire <kjaleshire@gmail.com>
2014-12-18 12:48:33 -0600

    add markdown rendering directory

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "net/http"
+import (
+    "net/http"
+    "os"
+
+    "github.com/russross/blackfriday"
+)
 
 func main() {
-    http.ListenAndServe(":8080", http.FileServer(http.Dir('.')))
+    port := os.Getenv("PORT")
+    if port = "" {
+        port = "8080"
+    }
+
+    http.HandleFunc("/markdown" GenerateMarkdown)
+    http.Handle("/", http.FileServer(http.Dir("public")))
+    http.ListenAndServe(":" + port, nil)
+}
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+    markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+    rw.Write(markdown)
 }
